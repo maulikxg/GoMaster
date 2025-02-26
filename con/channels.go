4a1c0fa9df50da6d54c4bf674be654603f944d4e
maulikxg <maulikgoswami7021@gmail.com>
2025-02-26 15:29:01 +0530

    string opretions

--- con/channels.go
+++ con/channels.go
@@ -2,21 +2,97 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
+type Order struct {
+	ID     int
+	Status string
+}
+
+func generateOrders(count int) []*Order {
+
+	orders := make([]*Order, count)
+
+	for i := 0; i < count; i++ {
+		orders[i] = &Order{ID: i + 1, Status: "pending"}
+	}
+
+	return orders
+
+}
+
+func processOrder(orders []*Order) {
+
+	for _, order := range orders {
+
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+
+		fmt.Printf("Processing order %d\n", order.ID)
+
+	}
+}
+
+func updateOrderStatus(orders []*Order) {
+
+	for _, order := range orders {
+
+		time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond)
+
+		status := []string{"Processing", "Shipped", "Delivered"}[rand.Intn(3)]
+
+		order.Status = status
+
+		fmt.Printf("Updated order %d status :  %s\n", order.ID, order.Status)
+
+	}
+}
+
+func reportOrderStatus(orders []*Order) {
+
+	for i := 0; i < 5; i++ {
+
+		time.Sleep(1 * time.Second)
+
+		fmt.Println("\n---- Order Status Report ----")
+
+		for _, order := range orders {
+			fmt.Printf("Order %d : %s\n", order.ID, order.Status)
+		}
+
+		fmt.Println("---------------------------------\n")
+
+	}
+
+}
+
 func main() {
-	ch := make(chan string)
+
+	var wg sync.WaitGroup
+
+	orders := generateOrders(10)
+
+	wg.Add(3)
 
 	go func() {
-		time.Sleep(2 * time.Second)
-		ch <- "Hello"
+		defer wg.Done()
+		processOrder(orders)
 	}()
 
-	select {
-	case msg := <-ch:
-		fmt.Println("Received:", msg)
-	case <-time.After(1 * time.Second):
-		fmt.Println("Timeout! No message received")
-	}
+	go func() {
+		defer wg.Done()
+		updateOrderStatus(orders)
+	}()
+
+	go func() {
+		defer wg.Done()
+		reportOrderStatus(orders)
+	}()
+
+	wg.Wait()
+
+	fmt.Println("All the orders completed  Exiting!")
+
 }
--- strings/main.go
+++ strings/main.go
@@ -7,8 +7,76 @@ import (
 
 func main() {
 
-	q := []string{"yo", "ho", "maulik", "heloo"}
-	r := strings.Join(q, "/")
-	fmt.Println(r)
+	//q := []string{"yo", "ho", "maulik", "heloo"}
+	//r := strings.Join(q, "/")
+	//fmt.Println(r)
+
+	//a := "go,java,hwa"
+	//b := strings.Split(a, ",")
+	//fmt.Println(b)
+
+	//c := "    Hello !! Go   "
+	//d := strings.Fields(c)
+	//fmt.Println(d)
+
+	//fmt.Println(strings.Contains("golang", "go"))
+	//fmt.Println(strings.ContainsAny("team", "t"))
+	//fmt.Println(strings.ContainsAny("   ", " "))
+
+	//fmt.Println(strings.HasPrefix("golang", "Go"))
+	//fmt.Println(strings.HasSuffix("golang", "lang"))
+
+	//fmt.Println(strings.Index("golang", "lang"))
+	//fmt.Println(strings.LastIndex("golang", "Go"))
+	//fmt.Println(strings.Count("golanggo", "go"))
+
+	//s := "Go is fast , Go is powerful"
+	//fmt.Println(strings.Replace(s, "Go", "Golang", 1))
+	//fmt.Println(strings.ReplaceAll(s, "Go", "Golang"))
+
+	//fmt.Println(strings.ContainsRune("tems", 97))
+	//fmt.Println(strings.ContainsRune("teams", 97))
+
+	//fmt.Println(strings.Count("efive", "e"))
+	//fmt.Println(strings.Count("five", ""))
+
+	//Cut
+	/*	show := func(s, sep string) {
+
+			before, after, found := strings.Cut(s, sep)
+
+			fmt.Printf("Cut(%q , %q) = %q , %q , %v)\n", s, sep, before, after, found)
+
+		}
+
+		show("Gopher", "Go")
+		show("Gopher", "ph")
+		show("Gopher", "er")
+		show("Gopher", "Badger") */
+
+	// fmt.Println(strings.EqualFold("ab", "AB"))
+	//
+	//s := []string{"tu", "hai", "to", "me"}
+	//r := strings.Join(s, "   ")
+	//fmt.Println(r)
+
+	//fmt.Println(strings.LastIndex("go langgogl", "go"))
+	//fmt.Println(strings.LastIndex("go golang", "go"))
+	//fmt.Println(strings.LastIndex("go gopher", "ra"))
+
+	//fmt.Println(strings.LastIndexAny("go gopher", "Gg"))
+	//fmt.Println(strings.LastIndexAny("go gopher", "rodent"))
+	//fmt.Println(strings.LastIndexAny("go gopher", "gail"))
+
+	// fmt.Println("ba" + strings.Repeat("nana", 3))
+
+	//fmt.Println(strings.Replace("maulik maulik maulik", "mau", "mo", -1))
+
+	//fmt.Println(strings.ReplaceAll("maulik maulik maulik", "li", "lo"))
+
+	//fmt.Printf("%q\n", strings.SplitAfter("a,b,c", ","))
+	//fmt.Printf("%q\n", strings.SplitAfterN("a,b,c", ",", 2))
+
+	fmt.Println(strings.Trim("maulikmaulikma", "ma"))
 
 }
